c58c490c76ae96a233b62f305cacc974c3feacc8
Enfernuz <github.colog@aleeas.com>
2025-06-07 20:48:44 +0400

    Pass #WY1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::io::Read;
 use std::io::Write;
+use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                _stream.write_all(b"+PONG\r\n").unwrap();
+            Ok(mut stream) => {
+                let mut buf: [u8; 256] = [0; 256];
+                loop {
+                    let read_count: usize = stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
