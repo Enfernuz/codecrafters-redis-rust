9d5897c9624d0560bd0f5d080e0c53cac90d40ea
Enfernuz <github.colog@aleeas.com>
2025-06-07 21:22:48 +0400

    Pass ZU#2

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,28 @@
 use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
+use std::thread;
+
+use std::net::TcpStream;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf: [u8; 256] = [0; 256];
-                loop {
-                    let read_count: usize = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        thread::spawn(|| match stream {
+            Ok(stream) => handle_connection(stream),
+            Err(e) => println!("error: {}", e),
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf: [u8; 256] = [0; 256];
+    loop {
+        let read_count: usize = stream.read(&mut buf).unwrap(); // blocking read
+        if read_count == 0 {
+            break;
         }
+        stream.write(b"+PONG\r\n").unwrap();
     }
 }
